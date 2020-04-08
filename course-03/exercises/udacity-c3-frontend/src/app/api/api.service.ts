7fc8ebf584aa10b6cba22760ac09b9544028f17f
Perez, David <David.Perez2@adidas.com>
2020-04-08 14:38:36 +0200

    Solving issue while retrieving AWS_PROFILE

--- course-03/exercises/udacity-c3-frontend/src/app/api/api.service.ts
+++ course-03/exercises/udacity-c3-frontend/src/app/api/api.service.ts
@@ -55,6 +55,7 @@ export class ApiService {
     const signed_url = (await this.get(`${endpoint}/signed-url/${file.name}`)).url;
 
     const headers = new HttpHeaders({'Content-Type': file.type});
+
     const req = new HttpRequest( 'PUT', signed_url, file,
                                   {
                                     headers: headers,
